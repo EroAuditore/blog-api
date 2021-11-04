6224d56fb7972065b6aac588f34040646efad00f
Daniel Samuel <dansam5000@gmail.com>
2021-11-04 22:40:14 +0100

    feat: Add create comment endpoint

--- app/controllers/api/v1/comments_controller.rb
+++ app/controllers/api/v1/comments_controller.rb
@@ -1,14 +1,33 @@
 class Api::V1::CommentsController < ApplicationController
      before_action :authenticate_user!
-    def index
-        @post = Post.find(params[:id]).comments
+  def index
+    @post = Post.find(params[:id]).comments
        
-        render json: {
-            data: @post,
-            # user: current_user.posts,
-            status: 200,
-            message: "All post"
-          }, status: :ok
+    render json: {
+      data: @post,
+      # user: current_user.posts,
+      status: 200,
+      message: "All post"
+    }, status: :ok
+  end
+
+    # Add comment for post
+  def create
+    @comment = Comment.new(comment_params)
+    @comment.post_id = params[:id]
+    @comment.user = current_user
+
+    if @comment.save
+      render json: {
+        status: :created,
+        message: 'Comments created'
+      }, status: :created
+    else
+      render json: {
+        status: :not_acceptable,
+        message: 'Comments not created'
+      }, status: :not_acceptable
     end
+  end
     
 end

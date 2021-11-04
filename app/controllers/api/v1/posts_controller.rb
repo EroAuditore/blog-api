d101d30d7c925468e6f990b555734ce82f69275f
eroauditore <alfa.fenrir@gmail.com>
2021-11-04 14:39:25 -0600

    End points to create/retrieve posts

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -9,5 +9,28 @@ class Api::V1::PostsController < ApplicationController
           }, status: :ok
     end
     
+    def create
+        @post = current_user.posts.new(post_params)
+    
+        if @post.save
+            render json: {
+                status: :created,
+                message: "Post was successfully created."
+              }, status: :created
+         
+        else
+            render json: {
+                status: :created,
+                message: "Post was not created."
+              }, status: :created
+        end
+      end
+
+    private
+  
+
+    def post_params
+        params.require(:post).permit(:content)
+    end
   
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       get '/posts', to: 'posts#index'
+      post '/posts', to: 'posts#create'
       get '/posts/:id/comments', to: 'comments#index'
       post '/posts/:id/comments', to: 'comments#create'
     end

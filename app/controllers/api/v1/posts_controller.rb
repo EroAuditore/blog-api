229e1c3f886e5fba220b3e1977dea6c467b8135b
Daniel Samuel <dansam5000@gmail.com>
2021-11-04 22:43:49 +0100

    feat: Add create post api-enpoint

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -10,5 +10,20 @@ class Api::V1::PostsController < ApplicationController
           }, status: :ok
     end
 
-  
+    def create
+    @post = current_user.posts.new(post_params)
+
+    if @post.save
+      render json: {
+        status: :created,
+        message: 'Post was successfully created.'
+      }, status: :created
+
+    else
+      render json: {
+        status: :created,
+        message: 'Post was not created.'
+      }, status: :created
+    end
+  end
 end

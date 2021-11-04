51ad1405e6008d099d613a16b12a535b6a5d4882
eroauditore <alfa.fenrir@gmail.com>
2021-11-04 14:12:18 -0600

    Initial routes added

--- app/controllers/api/v1/comments_controller.rb
+++ app/controllers/api/v1/comments_controller.rb
@@ -1,22 +1,14 @@
-class Api::V1::PostsController < ApplicationController
+class Api::V1::CommentsController < ApplicationController
      before_action :authenticate_user!
     def index
-        @posts = Post.all
+        @post = Post.find(params[:id]).comments
        
         render json: {
-            
+            data: @post,
+            # user: current_user.posts,
             status: 200,
             message: "All post"
           }, status: :ok
     end
-
-    def comments
-        @comments = Post.last.comments        
-       
-        render json: {
-            data: @comments,
-            status: 200,
-            message: "Comments for the post"
-          }, status: :ok
-    end
+    
 end

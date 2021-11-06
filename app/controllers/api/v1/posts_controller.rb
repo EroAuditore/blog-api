cb7a43d24dab0716354309a3ae30ea1e7954d224
eroauditore <alfa.fenrir@gmail.com>
2021-11-05 19:40:33 -0600

    med

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -1,7 +1,7 @@
 module Api
   module V1
     class PostsController < ApplicationController
-      before_action :authenticate_user!
+      # before_action :authenticate_user!
       def index
         @posts = Post.all
 
